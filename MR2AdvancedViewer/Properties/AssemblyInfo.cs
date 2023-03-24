6c79fa97749173a8e1ca19aaa866d28b44aaba98
Alexia Daniels <lexichu15@gmail.com>
2023-03-24 15:12:40 +0000

    Added "copyright info", corrected the version info.
    Signed-off-by: Alexia Daniels <lexichu15@gmail.com>

--- MR2AdvancedViewer/Properties/AssemblyInfo.cs
+++ MR2AdvancedViewer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MR2AdvancedViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Monster Rancher 2 Advanced Viewer")]
+[assembly: AssemblyDescription("A tool for viewing data about Monster Rancher/Farm 2.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MR2AdvancedViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Lexichu")]
+[assembly: AssemblyProduct("MR2AV")]
+[assembly: AssemblyCopyright("Can I even copyright this?")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.1.1")]
+[assembly: AssemblyFileVersion("0.7.1.1")]
